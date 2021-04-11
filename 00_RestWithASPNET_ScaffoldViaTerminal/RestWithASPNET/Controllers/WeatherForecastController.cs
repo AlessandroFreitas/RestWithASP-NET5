a293d794824f279e47bc2c53d3f2df966fb01fae
Alessandro Freitas <AlessandroFreitas@users.noreply.github.com>
2021-04-11 10:56:28 -0300

    Update .gitignore and delete cached files

--- 00_RestWithASPNET_ScaffoldViaTerminal/RestWithASPNET/Controllers/WeatherForecastController.cs
+++ 00_RestWithASPNET_ScaffoldViaTerminal/RestWithASPNET/Controllers/WeatherForecastController.cs
@@ -26,12 +26,12 @@ namespace RestWithASPNET.Controllers
         [HttpGet]
         public IEnumerable<WeatherForecast> Get()
         {
-            var rng = new Random();
+            var RNG = new Random();
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
             {
                 Date = DateTime.Now.AddDays(index),
-                TemperatureC = rng.Next(-20, 55),
-                Summary = Summaries[rng.Next(Summaries.Length)]
+                TemperatureC = RNG.Next(-20, 55),
+                Summary = Summaries[RNG.Next(Summaries.Length)]
             })
             .ToArray();
         }
